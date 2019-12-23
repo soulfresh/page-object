2d530dc047bff4ea2681ec5d265bc1056c1c7d52
Marc Wren <things@soul-fresh.com>
2019-12-23 11:08:24 -0700

    Build sourcemaps for distribution.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
@@ -17,10 +15,12 @@ export default [
       {
         file: pkg.main,
         format: 'cjs',
+        sourcemap: true,
       },
       {
         file: pkg.module,
         format: 'es',
+        sourcemap: true,
       }
     ],
     plugins: [
